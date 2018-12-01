95b30a60c88f22bf293c9939544c2ac3f0e09acb
MickMelon <michael.mcmillan@outlook.com>
2018-12-01 17:45:39 +0000

    Clean controllers array

--- app/Base/Router.php
+++ app/Base/Router.php
@@ -8,12 +8,13 @@ class Router
     /**
      * All controllers and their actions are contained in this array.
     */
-    private $controllers = array('page' => ['error', 'rss'],
-                         'article' => ['index', 'single', 'create', 'submit_create', 'upload_image'],
-                         'evaluation' => ['index'],
-                         'login' => ['index', 'login', 'logout', 'success'],
-                         'register' => ['index', 'register', 'success'],
-                         'sensor' => ['store', 'store_test']);
+    private $controllers = array(
+        'page' => ['error', 'rss'],
+        'article' => ['index', 'single', 'create', 'submit_create', 'upload_image'],
+        'evaluation' => ['index'],
+        'login' => ['index', 'login', 'logout', 'success'],
+        'register' => ['index', 'register', 'success'],
+        'sensor' => ['store', 'store_test']);
 
     public function start()
     {
